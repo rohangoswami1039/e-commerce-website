deef48f20a9495d476f1b1a02f71cb2d50d65d07
Rohan Goswami <rohangoswami1039@gmail.com>
2022-06-10 13:56:46 +0530

    title_bar

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,9 +1,26 @@
+//Importing all the modules 
 import React from 'react';
 import '../styles/main.css'
+import logo from '../../assets/logo.png'
+
+
 function App() {
   return (
     <div className="App">
-        Home Page
+      <div className='Title_bar'>
+        <div className='Logo'>
+            <img className='logo_image' src={logo} width={50} height={50}/>
+            <div className='logo_text'>Thirdweel</div>
+        </div>
+        <div className='Links'>
+            <div className='home'>Home</div>
+            <div className='about_us'>About Us</div>
+            <div className='hiring'>Hiring</div>
+            <div className='contact_us'>Contact Us</div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
